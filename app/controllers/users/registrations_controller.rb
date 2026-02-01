13e1c58247d4888be7914a5fa99f4deab180947a
Ber-Zer-Ka <nikokatamadze40@gmail.com>
2026-02-02 00:43:18 +0400

    Fix translation missing and prevent Gmail email collapsing

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -9,7 +9,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
       # SILENT RE-SEND: Update password (in case of typo) and resend the link
       existing_user.update(password: sign_up_params[:password], password_confirmation: sign_up_params[:password_confirmation])
       existing_user.send_confirmation_instructions
-      set_flash_message! :notice, :send_instructions
+      set_flash_message! :notice, :signed_up_but_unconfirmed
       respond_with resource, location: after_inactive_sign_up_path_for(resource)
     else
       # Normal behavior for new users or already confirmed users
