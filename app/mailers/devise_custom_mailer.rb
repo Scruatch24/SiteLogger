ca869af5683f3f7b470ef3f6a9ef23bd03c446bc
Ber-Zer-Ka <nikokatamadze40@gmail.com>
2026-02-13 08:49:22 +0400

    Add boxed card layout to Devise emails, set card background to #FAF9F5

--- app/mailers/devise_custom_mailer.rb
+++ app/mailers/devise_custom_mailer.rb
@@ -2,6 +2,7 @@ class DeviseCustomMailer < Devise::Mailer
   helper :application # gives access to all helpers defined within `application_helper`.
   include Devise::Controllers::UrlHelpers # Optional. eg. `confirmation_url`
   default from: "TalkInvoice <contact@talkinvoice.online>"
+  layout "mailer"
 
   def confirmation_instructions(record, token, opts = {})
     opts[:subject] = I18n.t("devise.mailer.confirmation_instructions.subject", ref: SecureRandom.hex(3).upcase)
