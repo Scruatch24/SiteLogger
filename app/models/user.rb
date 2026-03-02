1ad3325326a1bdbb5b9d79c20eb411600ff0a068
Ber-Zer-Ka <nikokatamadze40@gmail.com>
2026-03-02 14:09:13 +0400

    Fix Devise lockable configuration - move options to initializer

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable,
-         :lockable, lock_strategy: :failed_attempts, unlock_strategy: :time,
-         maximum_attempts: 10, unlock_in: 15.minutes,
+         :lockable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   has_many :logs, dependent: :destroy
